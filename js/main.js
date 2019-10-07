ab5e204a88acb0ff06ab5a87f0f13cfec40a0ed3
Lucian Virtic <lucian.virtic@iquestgroup.com>
2019-10-07 11:38:44 +0300

    v.0.1

---
+++ js/main.js
@@ -0,0 +1,18 @@
+x = 0;
+theta = 0;
+function setup()
+{
+  createCanvas(300,300,WEBGL);
+  s = createSlider(0,255,100);
+  s.position(10,10);
+  bro = loadModel('../assets/bro-3d.obj');
+}
+function draw()
+{
+  background(100);
+  //theta++;
+  //rotateX(theta*0.1);
+  ambientLight(100,0,0);
+  //box(100);
+  model(bro);
+}
